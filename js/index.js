ac09be1140c9e99c8ab5a7d5858b657c33627fba
overkils <tarasevich.daniils@gmail.com>
2021-07-16 21:00:00 +0300

    compleatHead

--- js/index.js
+++ js/index.js
@@ -20,9 +20,36 @@ for (let i = 0; i < selectSingle_labels.length; i++) {
 }
 
 const $slider = $('.head__slider');
+const $progressBar = $('.progress');
+
+var $status = $('.pagingCurrent');
+var $pagingAll = $('.pagingAll');
+var $slickElement = $slider;
+
+function setProgress(index) {
+    const calc = ((index + 1) / ($slider.slick('getSlick').slideCount)) * 100;
+    $progressBar
+        .css('background-size', `${calc}% 100%`)
+        .attr('aria-valuenow', calc);
+}
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    // $status.text(i + '/' + slick.slideCount);
+    $status.text('0' + i);
+    $pagingAll.text('0' + slick.slideCount);
+});
+
+$slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    setProgress(nextSlide);
+});
 
 $slider.slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
+    prevArrow: $('.slickPrev'),
+    nextArrow: $('.slickNext'),
 });
+
+setProgress(0);
