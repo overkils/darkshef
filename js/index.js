51be134d7cbdb03e6437f68f7625e90ca9d22804
overkils <tarasevich.daniils@gmail.com>
2021-08-10 13:56:13 +0300

    addd

--- js/index.js
+++ js/index.js
@@ -16,8 +16,20 @@ for (let i = 0; i < selectSingle_labels.length; i++) {
     selectSingle_labels[i].addEventListener('click', (evt) => {
         selectSingle_title.textContent = evt.target.textContent;
         selectSingle.setAttribute('data-state', '');
+        Cookies.set('sity', selectSingle_title.textContent);
     });
+};
+
+console.log(Cookies.get('sity'));
+
+var blocks = document.querySelector('.header__select-title');
+if (Cookies.get('sity') == undefined) {
+    blocks.textContent = "Минск";
+} else {
+    blocks.textContent = Cookies.get('sity');
 }
+
+console.log(blocks);
 
 const $slider = $('.head__slider');
 const $progressBar = $('.progress');
@@ -53,3 +65,41 @@ $slider.slick({
 });
 
 setProgress(0);
+
+$('.calendar__items').slick({
+    slidesToShow: 7,
+    slidesToScroll: 3,
+    arrows: false,
+    dots: false,
+    infinite: true,
+});
+
+const selectSingles = document.querySelector('.menu-header__select');
+const selectSingle_titles = selectSingles.querySelector('.menu-header__select-title');
+const selectSingle_labelss = selectSingles.querySelectorAll('.menu-header__select-label');
+
+// Toggle menu
+selectSingle_titles.addEventListener('click', () => {
+    if ('active' === selectSingles.getAttribute('data-state')) {
+        selectSingles.setAttribute('data-state', '');
+    } else {
+        selectSingles.setAttribute('data-state', 'active');
+    }
+});
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labelss.length; i++) {
+    selectSingle_labelss[i].addEventListener('click', (evt) => {
+        selectSingle_titles.innerHTML = evt.target.innerHTML;
+        selectSingles.setAttribute('data-state', '');
+        Cookies.set('eat', selectSingle_titles.innerHTML);
+    });
+};
+
+
+
+if (Cookies.get('eat') == undefined) {
+    selectSingle_titles.innerHTML = "<span class='menu-header__title'>Офис<span class='menu-header__kcal'>900 калл.</span></span>";
+} else {
+    selectSingle_titles.innerHTML = Cookies.get('eat');
+}
